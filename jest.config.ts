b22af60334ed337cb0c3efa16ead814ea35934ec
KaalebCD <kaleb.damaceno@gmail.com>
2023-07-15 21:50:35 -0300

    `chore:` add `jest` to `lint-staged`

--- jest.config.ts
+++ jest.config.ts
@@ -1,16 +1,16 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
 
 import type { Config } from "jest";
 
 const config: Config = {
-  roots: ['<rootDir>/src'],
+  testEnvironment: "node",
+  roots: ["<rootDir>/src"],
   collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
   coverageDirectory: "coverage",
-  testEnvironment: "jest-environment-node",
-  transform: {
-    ".+\\.ts$": "ts-jest",
-  }
-
-
+  transform: { "^.+\\.ts$": "ts-jest" },
 };
 
 export default config;
